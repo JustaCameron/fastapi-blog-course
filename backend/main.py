259614bcb137582f06a578ae8beb259a1e2ff693
JustaCameron <cameron.meeks@speurgroup.com>
2023-07-18 14:45:01 -0500

    Connection to Database

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
 from core.config import settings
+from db.session import engine
+from db.base_class import Base
 
-app = FastAPI(title=settings.PROJECT_TITLE, verison=settings.PROJECT_VERSION)
+def create_tables():
+    Base.metadata.create_all(bind=engine) # Connects past API to database engine
+
+def start_application():
+    app = FastAPI(title=settings.PROJECT_TITLE, version=settings.PROJECT_VERSION)
+    create_tables()
+    return app
+
+
+app = start_application()
 
 @app.get("/")
 def hello():
-    return {"msg": "Hello FastAPI"}
+    return {"detail": "Hello FastAPI"}
+
